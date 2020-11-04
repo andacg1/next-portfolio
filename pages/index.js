408b872c8a7a3648af459ca3afaacc7d03937599
Deniz Gunay <69558826+deniz-tbs@users.noreply.github.com>
2020-11-04 03:52:42 -0800

    feat: first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,41 @@
+import {useEffect, useState, useRef, useCallback} from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {Hero} from "../components/styles";
+import {motion, useViewportScroll} from "framer-motion"
+import * as THREE from 'three';
+import {Canvas, extend, FogExp2, useFrame} from "react-three-fiber";
+import Box from "../components/three/Box";
+import Image from 'next/image'
+import Particles from "../components/three/Particles";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const {scrollYProgress, scrollY} = useViewportScroll()
+    const [down, set] = useState(false)
+    const mouse = useRef([0, 0])
+    const onMouseMove = useCallback(({ clientX: x, clientY: y }) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), [])
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    useEffect(() => {
+        console.log(scrollY)
+        console.log(scrollYProgress)
+    }, [scrollY])
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    return (
+        <Canvas
+            onMouseMove={onMouseMove}
+            onMouseUp={() => set(false)}
+            onMouseDown={() => set(true)}
+            style={{width: '100vw', height: '100vh', backgroundImage: 'url("outrun.jpg")', backgroundPosition: 'center', backgroundSize: 'cover'}}>
+            <ambientLight/>
+            <pointLight position={[10, 10, 10]} />
+            <fogExp2 attach="fog" args={['white', 50]} density={0.3}/>
+            <Particles count={5000} mouse={mouse} />
+            <Box position={[0, 1.2, 3]} scale={[1, 1, 1]} />
+            <Box position={[-1.2, 0, 2]} scale={[1, 1, 1]}/>
+            <Box position={[0, -1.2, -3]} scale={[1, 1, 1]} />
+            <Box position={[0, -2.0, 0]} scale={[0.4, 0.4, 0.4]}/>
+            <Box position={[0, -2.4, 0]} scale={[0.2, 0.2, 0.2]}/>
+            <Box position={[1.2, 0, 2]} scale={[1, 1, 1]}/>
+        </Canvas>
+    )
 }
